b8903da9cb495239ab90efebdbbbcb9f06e5dff1
Güven Fazlı <guven.fazli@gmail.com>
2023-12-17 22:46:09 +0300

    pop up with style added

--- theater/src/Components/Welcome.jsx
+++ theater/src/Components/Welcome.jsx
@@ -2,6 +2,8 @@ import { forwardRef, useRef } from "react"
 
 const Welcome = forwardRef(function Welcome({close}, ref){
   return (
+    <>
+    <div className="karartma"></div>
     <dialog ref={ref}>
  
       <h1>Welcome to my test page, i do many tests here...</h1>
@@ -11,6 +13,7 @@ const Welcome = forwardRef(function Welcome({close}, ref){
       </form>
     
     </dialog>
+    </>
   )
 })
 
