bbec521dd35727ae801875b65916bbd90a7a99cd
Güven Fazlı <guven.fazli@gmail.com>
2023-12-20 03:57:53 +0300

    Buttons removed from completed tasks

--- theater/src/Components/CompleteTasks.jsx
+++ theater/src/Components/CompleteTasks.jsx
@@ -1,6 +1,6 @@
 import RemovePopUp from "./Remove"
 
-export default function CompleteTasks({work, date, removePup, remove, removeWork, complete}){
+export default function CompleteTasks({work, date}){
   return (
   
   <>
@@ -14,15 +14,6 @@ export default function CompleteTasks({work, date, removePup, remove, removeWork
           <p>
             {date}
           </p>
-
-          <RemovePopUp open={removePup}>
-            <button onClick={remove}>Yes</button>
-            <button onClick={removeWork}>No</button>
-          </RemovePopUp>
-          
-          <button onClick={removeWork} className='remove-btn'>Remove</button>
-          <button onClick={complete} className='complete-btn'>Complete</button>
-
         </div>
     
 
