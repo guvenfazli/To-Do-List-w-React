631a659f74ecc8696f353d22e20d9f012b234403
Güven Fazlı <guven.fazli@gmail.com>
2023-12-23 14:15:48 +0300

    Style fixed

--- theater/src/Components/RenderWork/RenderWork.jsx
+++ theater/src/Components/RenderWork/RenderWork.jsx
@@ -6,7 +6,7 @@ export default function RenderWork({work, date, description, removeTask, markAsC
        
         <div className={`${removeAnim ? 'work' : 'remove-task-animation'} ${!ex ? 'work' : 'expanded-div'}`}>
           <p>{work}</p>
-          <p>{date}</p>
+          <p style={{flexShrink: '0'}}>{date}</p>
           <button onClick={removeTask} className='remove-btn'>Remove</button> 
           <button onClick={markAsCompleted} className='complete-btn'>Complete</button>   
           <button className="expand-btn" onClick={expand}>Description</button> 
